52eab8cd53cca9cd1b121db7e3e3ffb5c5216abd
veronikapodgol <veronika.podgol@gmail.com>
2022-06-12 14:20:32 +0300

    add new tests

--- src/test/java/OrderStepsTest.java
+++ src/test/java/OrderStepsTest.java
@@ -1,4 +1,5 @@
 import com.codeborne.selenide.SelenideElement;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
@@ -12,6 +13,76 @@ import static java.lang.Thread.sleep;
 
 @Tag("ABO")
 public class OrderStepsTest extends TestBase {
+    @Test
+    void checkAbilityToMakeOrder() {
+        step("Open page", () -> {
+            open("");
+            $("#cmpwelcomebtnyes").click();
+        });
+        step("Check Abo bestellen item", () -> {
+            $("#nav_text_collapse").shouldHave(text("Abo bestellen"));
+        });
+    }
+
+    @Test
+    void opportunityToGoToOrderPage() {
+        step("Open page", () -> {
+            open("");
+            $("#cmpwelcomebtnyes").click();
+            $("#nav_text_collapse").$(byText("Abo bestellen")).click();
+            sleep(5000);
+        });
+        step("Checking necessary order page", () -> {
+            $(".wrapper").shouldHave(text("BVG-Abo online bestellen"));
+        });
+    }
+
+    @Test
+    void checkAvailableZone() {
+        step("Open page", () -> {
+            open("");
+            $("#cmpwelcomebtnyes").click();
+        });
+        step("Choose the ticket", () -> {
+            $("#nav_text_collapse").$(byText("Abo bestellen")).click();
+            sleep(5000);
+            $$(".card").findBy(text("VBB-Umweltkarte")).$("button").click();
+        });
+        step("Check necessary Zone Berlin ABC", () -> {
+            $(".button-list-block").$(byText("Berlin ABC")).click();
+             Assertions.assertEquals("Berlin ABC", $(".sub-row-item").$("input").getValue());
+        });
+
+    }
+    @Test
+    void checkTheZipCity(){
+        step("Open page", () -> {
+            open("");
+            $("#cmpwelcomebtnyes").click();
+        });
+        step("Choose the ticket", () -> {
+            $("#nav_text_collapse").$(byText("Abo bestellen")).click();
+            sleep(5000);
+            $$(".card").findBy(text("VBB-Umweltkarte")).$("button").click();
+        });
+        step("Select price options", () -> {
+            $(".button-list-block").$(byText("Berlin ABC")).click();
+            $(".price-next-step").$("button").click();
+        });
+        step("Fill Personal data", () -> {
+            SelenideElement title = $$(".row").findBy(text("Anrede"));
+            /*$("#vs6__combobox").*/
+            title.$("[role=combobox]").$("[placeholder='Bitte wählen']").sendKeys("");
+            title.$("[role=listbox]").$(byText("Herr")).click();
+            $("[name=firstName]").setValue("Andy");
+            $("[name=lastName]").setValue("Rgjf");
+            $("[name=birthday]").setValue("01.02.2000");
+
+            SelenideElement zipCity = $$(".row").findBy(text("PLZ"));
+            zipCity.$("[role=combobox]").$("[placeholder='PLZ auswählen / Ort auswählen']").setValue("30159");
+            zipCity.$("[role=listbox]").shouldHave(text("30159 Hannover"));
+        });
+    }
 
     @Test
     void makeOrderTest() throws InterruptedException {
@@ -58,6 +129,7 @@ public class OrderStepsTest extends TestBase {
             $("[name=iban]").setValue("DE46500105175963927676");
             $(".row-check").$(".checkbox-component").click();
             $("#nextStep").click();
+            $("#kc-page-title").shouldHave(text("Anmelden"));
         });
     }
 }
